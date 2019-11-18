2a5515fad842881d00e0cc660a3e3137f54cf985
sam.lu <lsj@com>
2019-11-18 17:10:36 +0800

    触发重构

--- Assets/Scripts/SharedSystems/PresentationSystemGroup.cs
+++ Assets/Scripts/SharedSystems/PresentationSystemGroup.cs
@@ -15,6 +15,7 @@ namespace Assets.Scripts.ECS
             m_systemsToUpdate.Add(World.GetOrCreateSystem<UpdateCharPresentationSystem>());
             m_systemsToUpdate.Add(World.GetOrCreateSystem<ApplyCharPresentationSystem>());
             m_systemsToUpdate.Add(World.GetOrCreateSystem<ApplyItemPresentationSystem>());
+            m_systemsToUpdate.Add(World.GetOrCreateSystem<CharacterTriggerSystem>());
             m_systemsToUpdate.Add(World.GetOrCreateSystem<ClientTriggerProcessSystem>());      
             m_systemsToUpdate.Add(World.GetOrCreateSystem<UpdateUISystem>());
         }
