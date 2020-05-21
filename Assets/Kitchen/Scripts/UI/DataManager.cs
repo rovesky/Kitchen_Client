9901e6ad0d4d230cb4a9fbff4f62803e8906ea17
peter <414436452@qq.com>
2020-05-21 16:10:50 +0800

    [zero]* 添加烘焙 及 部分协议接入接口

--- Assets/Kitchen/Scripts/UI/DataManager.cs
+++ Assets/Kitchen/Scripts/UI/DataManager.cs
@@ -4,5 +4,15 @@ using UnityEngine;
 
 public class DataManager : BSingleton<DataManager>
 {
-
+    public LoginDataManager LoginDataManager { get; private set; }
+    public MainDataManager MainDataManager { get; private set; }
+    public RoomDataManager RoomDataManager { get; private set; }
+    public UserData UserData { get; private set; }
+    public void Init()
+    {
+        LoginDataManager = new LoginDataManager();
+        MainDataManager = new MainDataManager();
+        RoomDataManager = new RoomDataManager();
+        UserData = new UserData();
+    }
 }
